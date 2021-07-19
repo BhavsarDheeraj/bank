f980f4502d98a6d3e4b58277b9058f0694c08627
Dheeraj Bhavsar <dheerajbhavsar940@gmail.com>
2021-07-19 22:10:41 +0530

    Initial commit

---
+++ db/sqlc/store.go
@@ -0,0 +1,91 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+// Store provides all functions to execute db queries and transactions
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
+
+func (store *Store) executeTransaction(context context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(context, nil)
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx error: %v, rb error %v ", err, rbErr)
+		}
+		return err
+	}
+	return tx.Commit()
+}
+
+type TransferTxParams struct {
+	FromAccountId int64 `json:"fromAccountID"`
+	ToAccountId   int64 `json:"toAccountID"`
+	Amount        int64 `json:"amount"`
+}
+
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"fromAccount"`
+	ToAccount   Account  `json:"toAccount"`
+	FromEntry   Entry    `json:"fromEntry"`
+	ToEntry     Entry    `json:"toEntry"`
+}
+
+// TransferTx performs a money transaction from one account to the other.
+// It creates a new transfer record, add account entries, and update
+// accounts' balance within a single database transaction
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+
+	err := store.executeTransaction(ctx, func(q *Queries) error {
+		var err error
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountId,
+			ToAccountID:   arg.ToAccountId,
+			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountId,
+			Amount:    -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountId,
+			Amount:    arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	return result, err
+}
